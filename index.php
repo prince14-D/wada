e04271fb4a7173dea12ba9378acefc4596375f36
Prince W Dahn Jr <jaydahn278@gmail.com>
2025-10-09 20:56:36 +0530

    sss

--- index.php
+++ index.php
@@ -302,30 +302,30 @@
     <div class="row text-center g-4">
         <div class="col-md-3" data-aos="zoom-in">
             <div class="team-card rounded-4 shadow-lg p-3 hover-scale">
-                <img src="images/Ali Wleh Sackor.jpeg" alt="John Doe" class="img-fluid rounded-circle mb-3">
+                <img src="images/Ali Wleh Sackor.jpeg" alt="Prince D. Jegbadai" class="img-fluid rounded-circle mb-3">
                 <h5 class="fs-5 fw-bold">Prince D. Jegbadai</h5>
                 <p class="fs-5">Chief Executive Officer</p>
             </div>
         </div>
         <div class="col-md-3" data-aos="zoom-in" data-aos-delay="100">
             <div class="team-card rounded-4 shadow-lg p-3 hover-scale">
-                <img src="images/pic2.jpeg" alt="Jane Smith" class="img-fluid rounded-circle mb-3">
+                <img src="images/pic2.jpeg" alt="Ali Dackie" class="img-fluid rounded-circle mb-3">
                 <h5 class="fs-5 fw-bold">Ali Dackie</h5>
-                <p class="fs-5">Marine Engineer (BAM International)</p>
+                <p class="fs-5">Marine Engineer </p>
             </div>
         </div>
         <div class="col-md-3" data-aos="zoom-in" data-aos-delay="200">
             <div class="team-card rounded-4 shadow-lg p-3 hover-scale">
-                <img src="images/Elijah.jpg" alt="Michael Brown" class="img-fluid rounded-circle mb-3">
+                <img src="images/Elijah.jpg" alt="Elijah Ray Barclay" class="img-fluid rounded-circle mb-3">
                 <h5 class="fs-5 fw-bold">Elijah Ray Barclay</h5>
-                <p class="fs-5">Commercial Diver (DCN Global)</p>
+                <p class="fs-5">Commercial Diver</p>
             </div>
         </div>
         <div class="col-md-3" data-aos="zoom-in" data-aos-delay="300">
             <div class="team-card rounded-4 shadow-lg p-3 hover-scale">
-                <img src="images/pic4.jpeg" alt="Emily Davis" class="img-fluid rounded-circle mb-3">
+                <img src="images/pic4.jpeg" alt="Tom Kazouh" class="img-fluid rounded-circle mb-3">
                 <h5 class="fs-5 fw-bold">Tom Kazouh</h5>
-                <p class="fs-5">Environmental Specialist (DCN Global)</p>
+                <p class="fs-5">Environmental Specialist</p>
             </div>
         </div>
     </div>
